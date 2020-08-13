1db3f0ad865632aa36c84b8ae7be92a9f6968fae
Ramzi Nofal <ramzis.mailbox@gmail.com>
2020-08-13 13:08:09 +0100

    Inert User

--- pages/newuser.vue
+++ pages/newuser.vue
@@ -30,9 +30,10 @@
       v-model="form"
       class="pa-4 pt-6"
     >
-
+  
     <v-text-field
-        v-model="Name"
+        v-model="name"
+        :rules="[rules.required]"
         filled
         color="deep-purple"
         label="Name"
@@ -74,51 +75,23 @@
         class="white--text"
         color="deep-purple accent-4"
         depressed
+        @click="sendData()"
       >Submit</v-btn>
     </v-card-actions>
-    <v-dialog
-      v-model="dialog"
-      absolute
-      max-width="400"
-      persistent
-    >
-      <v-card>
-        <v-card-title class="headline grey lighten-3">Legal</v-card-title>
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn
-            text
-            @click="agreement = false, dialog = false"
-          >
-            No
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-btn
-            class="white--text"
-            color="deep-purple accent-4"
-            @click="agreement = true, dialog = false"
-          >
-            Yes
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+   
+
   </v-card>
 </template>
 
 <script>
   export default {
     data: () => ({
-      agreement: false,
-      dialog: false,
+      name: undefined,
       email: undefined,
       form: false,
       isLoading: false,
       password: undefined,
-      phone: undefined,
+      
       rules: {
         email: v => !!(v || '').match(/@/) || 'Please enter a valid email',
         length: len => v => (v || '').length >= len || `Invalid character length, required ${len}`,
@@ -127,8 +100,23 @@
         required: v => !!v || 'This field is required',
       },
     }),
-  }
 
+    methods: {
+      async sendData () {
+       this.$axios.post('insert', {
+          name: this.name, 
+          email: this.email,
+          password: this.password 
+        })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      }
+    }
+  }
 
 //<template>
   //<v-layout />
